14436271ae14a6d84070c495ee542c5a819ef4c8
lucienesaraujos <lucienesaraujos@gmail.com>
2026-04-27 02:16:43 -0300

    bootcamp

---
+++ Java/hello/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Configurando a JDK na versão " + args [0]);
+        System.out.println("Programação em " + args [1]);
+    }
+}
